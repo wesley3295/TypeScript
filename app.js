1baadceff5ee5797da6941848ac6c95e227e1e20
Wesley <wspencer3295@yahoo.com>
2021-04-30 00:05:54 -0700

    union types

--- app.js
+++ app.js
@@ -1,43 +1,13 @@
-//const person = {
-//    name:string;
-//    age:number;
-//} = {
-// TS infers the object attribute types.///////////////////////////////////////////////
-//     const person : {
-//         name:string;
-//         age:number;
-//         hobbies:string[]; // core types (number[],boolean[],string[])
-//         role:[number,string]; // we create the TS object types because with a Tuple we want be specific to what and how many types go into our array in role
-//     }={
-//     name:"Wesley",
-//     age:26,
-//     hobbies: ['Video Games', 'Fishing'],
-//     role:[2,'father'] // fixed length array Tuple
-// }
-// person.role.push('admin') //TS will not catch push functions. Be aware that this will not throw an error even though it should
-// person.role[1] = 10 // TS will throw error because index:1 is a string and cannot be a number, we declared that in our TS Object
-///////////////////////////////////////////////////////////////////////////////////////////////////
-var ADMIN = 0;
-var READ_ONLY = 1;
-var author = 2;
-var person = {
-    name: "Wesley",
-    age: 26,
-    hobbies: ['Video Games', 'Fishing'],
-    role: ADMIN
+//Union Types
+var combine = function (input1, input2) {
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        return input1 + input2;
+    }
+    else {
+        return input1.toString() + input2.toString();
+    }
 };
-//if your array is mixed types 'any[]'
-var favoriteActivities;
-favoriteActivities = ["Coding", "Games", "Home Improvement"];
-//!!ERRORS!!
-// favoriteActivities = ["Coding", "Games",1, "Home Improvement"] //ERROR cant have number
-// favoriteActivities = "Home Improvement" //ERROR has to be an array
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase()); // TS infers specific functions to call on specific types making TS more developer friendly
-    // console.log(hobby.map()); //ERROR TS knows that 'hobby' is a string there for cant 'map'
-}
-if (person.role === ADMIN) {
-    console.log('IS ADMIN');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Jax', 'Stickem');
+console.log(combinedNames);
